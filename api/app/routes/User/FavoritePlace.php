f894a977e44633f50f99c0d5156d87c06618b0e2
Ramonquii <q.castro1655@gmail.com>
2016-08-31 15:58:03 -0600

    ../setFavoritePlace
    Complete

--- api/app/routes/User/FavoritePlace.php
+++ api/app/routes/User/FavoritePlace.php
@@ -48,3 +48,72 @@ $app->get("/getFavoritePlace/:id", function($id) use($app){
 
     }
 });
+
+
+$app->post("/setFavoritePlace/", function() use($app){
+
+    $json = $app->request->getBody();
+    $data = json_decode($json, true);
+
+
+        $Latitude = $data['Latitude'];
+        $Longitude = $data['Longitude'];
+        $Name = $data['Name'];
+        $Description = $data['Description'];
+        $Client_Id = $data['Client_Id'];
+
+
+    $response = array();
+
+    try{
+
+        $connection = getConnection();
+        
+        $dbh = $connection->prepare("SELECT Name FROM Place_Favorite WHERE Name = :N ");
+        $dbh->bindParam(':N', $Name);
+        $dbh->execute();
+
+        $fav_place = $dbh->fetchObject();
+
+        if ($fav_place) {
+            
+
+            $connection = null;
+            $response['Message'] = "El lugar ya se encuentra registrado";
+            $response['IsError'] = true;
+            $response['Data'] = null;
+
+            $app->response->headers->set("Content-type", "application/json");
+            $app->response->status(400);
+            $app->response->body(json_encode($response));
+        }
+
+        
+        
+        else{
+                        
+            $dbh = $connection->prepare("INSERT INTO Place_Favorite (Latitude, Longitude, Name, Description, Client_Id) 
+                VALUES( :LN, :LG, :N, :D, :CID)");
+            $dbh->bindParam(':LN', $Latitude);
+            $dbh->bindParam(':LG', $Longitude);
+            $dbh->bindParam(':N', $Name);
+            $dbh->bindParam(':D', $Description);
+            $dbh->bindParam(':CID', $Client_Id);
+            $dbh->execute();
+
+            if ($dbh) {
+                $connection = null;
+                $response['Message'] = "Registrado Correctamente";
+                $response['IsError'] = false;
+                $response['Data'] = null;
+
+                $app->response->headers->set("Content-type", "application/json");
+                $app->response->status(400);
+                $app->response->body(json_encode($response));
+            }
+        }
+
+    }catch(PDOException $e){
+        echo "Error: " . $e->getMessage();
+    }
+});
